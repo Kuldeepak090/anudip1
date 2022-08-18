b9a1b2d89deab37d758a40f155c559d53014e6b0
Kuldeepak090 <95919923+Kuldeepak090@users.noreply.github.com>
2022-08-18 17:47:37 +0530

    commit

---
+++ pattern_number/src/pattern_number/pattern_number.java
@@ -0,0 +1,23 @@
+package pattern_number;
+
+public class pattern_number { 
+  
+	public static void main(String[] args)   
+	{  
+	int i, j, rows=4;  
+	for (i = 1; i <= rows; i++)   
+	{  
+	//Prints first half of the row  
+	for (j = 1; j <= i; j++)   
+	{   
+	System.out.print(j+" ");   
+	}  
+	//Prints second half of the row   
+	for (j = i-1; j >= 1; j--)  
+	{  
+	System.out.print(j+" ");  
+	}  
+	System.out.println();  
+	}  
+	}  
+	}  
